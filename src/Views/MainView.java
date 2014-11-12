b523422369909ff76ca03708c94c2fb07fb81d95
Rudie de Smit <rddesmit@hotmail.com>
2014-11-12 23:21:44 +0100

    first setup

---
+++ src/Views/MainView.java
@@ -0,0 +1,17 @@
+package Views;
+
+import javax.swing.*;
+
+/**
+ * Created by Rudie (rddesmit@hotmail.com) on 12-11-2014.
+ */
+public class MainView extends JFrame {
+
+    
+
+    public MainView() {
+        super("JDrone");
+
+        setSize(640, 360);
+    }
+}
